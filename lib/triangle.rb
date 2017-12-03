a0e7dedbd8eedddd17da2cb965808be09dcecd5f
Parker Catalano <catalano.work@gmail.com>
2017-12-03 18:29:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(sideA, sideB, sideC)
+    @sideA = sideA
+    @sideB = sideB
+    @sideC = sideC
+  end
+
+  def true
+    if @sideA > 0 && @sideB > 0 && @sideC > 0
+      true
+    end
+  end
+
+  def kind
+    if @sideA == @sideB && @sideB == @sideC
+      :equilateral
+    elsif @sideA == @sideB && @sideB !=
+    end
+  end
 end
